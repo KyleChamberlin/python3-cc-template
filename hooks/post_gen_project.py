4f49e7b4fdae3d1d14163074f7996aa96e93fcf0
Kyle Chamberlin <KyleChamberlin@project20million.org>
2015-03-10 12:27:10 -0400

    fixed hook import

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -5,16 +5,16 @@ import readline
 
 pip.main(['install', 'github3.py', '--user'])
 
-from github3 import login, Github
+from github3 import login, github
 
 user = '{{ cookiecutter.github_username }}'
 
 password = getpass('Github password for {0}: '.format(user))
 
-github = login(user, password)
+gh = login(user, password)
 
 repo = { 'name': '{{ cookiecutter.repo_name }}', 'description': '{{ cookiecutter.project_short_description }}' }
 
-success = github.create_repo(repo)
+success = gh.create_repo(repo)
 
 
