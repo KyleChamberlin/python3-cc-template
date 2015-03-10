9c9932cf40a5ad7052ba35a176fc51dc299e6a32
Kyle Chamberlin <KyleChamberlin@project20million.org>
2015-03-09 23:04:13 -0400

    first draft post hook

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,20 @@
+import sys
+import pip
+from getpass import getpass
+import readline
+
+pip.main(['install', 'github3.py'])
+
+from github3 import login, Github
+
+user = '{{ cookiecutter.github_username }}'
+
+password = getpass('Github password for {0}: '.format(user))
+
+github = login(user, password)
+
+repo = { 'name': '{{ cookiecutter.repo_name }}', 'description': '{{ cookiecutter.project_short_description }}' }
+
+success = github.create_repo(repo)
+
+
